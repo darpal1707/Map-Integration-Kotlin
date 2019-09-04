d00b3cd194e6a65fe0e4687ad760386ec4f8f427
Darpal <darpaldhyani2014@gmail.com>
2019-09-04 16:29:31 -0700

    static car listing near you on maps

--- app/src/main/java/com/example/kotlinmapintegration/MapsActivity.kt
+++ app/src/main/java/com/example/kotlinmapintegration/MapsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinmapintegration
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
@@ -10,19 +11,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.fragment.app.FragmentActivity
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import java.lang.Exception
 import java.util.jar.Manifest
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : FragmentActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+    private var mMap: GoogleMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,47 +35,55 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         checkPermissions()
+        LoadPockemon()
     }
-    var ACCESS_LOCATION = 123
+    var ACCESS_LOCATION=123
     fun checkPermissions(){
-        if(Build.VERSION.SDK_INT >=23){
-            if(ActivityCompat.checkSelfPermission(this,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION)
-                    != PackageManager.PERMISSION_GRANTED){
+
+        if(Build.VERSION.SDK_INT>=23){
+
+            if(ActivityCompat.
+                    checkSelfPermission(this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+
                 requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),ACCESS_LOCATION)
                 return
             }
         }
+
+        GetUserLocation()
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
+    fun GetUserLocation(){
+
+        var myLocation= MylocationListener()
+        var locationManager=getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,3,3f,myLocation)
+
+        var mythread=myThread()
+        mythread.start()
+        Toast.makeText(this,"User location " ,Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<out String>,
+                                            grantResults: IntArray) {
+
         when(requestCode){
-            ACCESS_LOCATION ->{
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    getUserLocation()
-                }
-                else{
-                    Toast.makeText(this, "Require location permission!",
-                        Toast.LENGTH_SHORT).show()
-                    checkPermissions()
+            ACCESS_LOCATION->{
+
+                if (grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    GetUserLocation()
+                }else{
+                    Toast.makeText(this,"We cannot access to your location",Toast.LENGTH_LONG).show()
                 }
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
-    fun getUserLocation(){
-        var currentLocation= MylocationListener()
-        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3, 3f, currentLocation)
-        val myThread=myThread()
-        myThread.start()
-        Toast.makeText(this, "User Location is: " + currentLocation, Toast.LENGTH_SHORT).show()
-    }
+
 
     /**
      * Manipulates the map once available.
@@ -85,63 +96,115 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
     }
 
     var location:Location?=null
-    //Fetch user location
-    inner class MylocationListener:LocationListener {
+
+    //Get user location
+    inner class MylocationListener:LocationListener{
 
         constructor(){
-            location = Location("Start")
-            location!!.longitude = 0.0
-            location!!.latitude = 0.0
-
+            location= Location("Start")
+            location!!.longitude=0.0
+            location!!.longitude=0.0
         }
-
         override fun onLocationChanged(p0: Location?) {
-             location = p0
+            location=p0
         }
 
         override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
-
+            //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         }
 
         override fun onProviderEnabled(p0: String?) {
-
+            // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         }
 
         override fun onProviderDisabled(p0: String?) {
-
+            //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         }
+
     }
 
-    inner class myThread: Thread{
+    var oldLocation:Location?=null
+    inner class myThread : Thread {
 
         constructor():super(){
-
+            oldLocation= Location("Start")
+            oldLocation!!.longitude=0.0
+            oldLocation!!.longitude=0.0
         }
+
         override fun run() {
             super.run()
             while (true){
                 try {
-                    runOnUiThread() {
+
+                    if(oldLocation!!.distanceTo(location)==0f){
+                        continue
+                    }
+                    oldLocation=location
+
+                    runOnUiThread {
+
                         mMap!!.clear()
+
+                        // show me
                         val sydney = LatLng(location!!.latitude, location!!.longitude)
-                        mMap.addMarker(
-                            MarkerOptions()
-                                .position(sydney)
-                                .title("My Location")
-                        )
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13f))
+                        mMap!!.addMarker(MarkerOptions()
+                            .position(sydney)
+                            .title("Me"))
+                        mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 14f))
+
+                        // show pockemons
+
+                        for(i in 0..listCars.size-1){
+
+                            var newCar=listCars[i]
+
+                            if(newCar.IsCatch==false){
+
+                                val pockemonLoc = LatLng(newCar.location!!.latitude, newCar.location!!.longitude)
+                                mMap!!.addMarker(MarkerOptions()
+                                    .position(pockemonLoc)
+                                    .title(newCar.name!!)
+                                    .icon(BitmapDescriptorFactory.fromResource(newCar.image!!)))
+
+
+                                if (location!!.distanceTo(newCar.location)<2){
+                                    newCar.IsCatch=true
+                                    listCars[i]=newCar
+                                    playerPower+=newCar.power!!
+                                    Toast.makeText(applicationContext,
+                                        "Your car is on the way to pick you! " + playerPower,
+                                        Toast.LENGTH_LONG).show()
+
+                                }
+
+                            }
+                        }
                     }
 
                     Thread.sleep(1000)
 
-                }catch (e:Exception){
-                    e.stackTrace
-                }
+                }catch (ex:Exception){}
+
             }
         }
+    }
+
+    var playerPower=0.0
+    var listCars=ArrayList<Car>()
+
+    fun  LoadPockemon(){
+
+
+        listCars.add(Car(R.drawable.icons8_car_24,
+            "Sedan", "Can fit max 4 people", 4.0, 37.7388065, -121.928364))
+        listCars.add(Car(R.drawable.icons8_car_24,
+            "Pool", "Can fit upto 2 peopel", 2.0, 37.7394076, -121.9313977))
+        listCars.add(Car(R.drawable.icons8_car_24,
+            "SUV", "Can fit max 6 people", 6.0, 37.7415178, -121.9318122))
+
     }
 }
